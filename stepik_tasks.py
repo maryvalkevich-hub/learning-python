bac300edf0f344be015b4c90ec3d92147c9792ad
Mary Valkevich <mary.valkevich@gmail.com>
2026-06-10 20:41:36 +0300

    Update stepik_tasks.py

--- stepik_tasks.py
+++ stepik_tasks.py
@@ -4,4 +4,4 @@ print(num, 2 * num, 3 * num, 4 * num, 5 * num, sep="---")
 
   # Задание №7
 num_a1, num_d, num_n = int(input()), int(input()), int(input())
-print(num_a1 + num_d*(num_n-1))
+print(num_a1 + num_d * (num_n-1))
