f9061952b222f8198105fada0b8b39e8b268c5f6
Mary Valkevich <mary.valkevich@gmail.com>
2026-06-10 20:27:00 +0300

    Create stepik_tasks.py

---
+++ stepik_tasks.py
@@ -0,0 +1,7 @@
+  #Задание #8
+num = int(input())
+print(num, 2*num, 3*num, 4*num, 5*num, sep="---")  
+
+  # Задание №7
+num_a1, num_d, num_n = int(input()), int(input()), int(input())
+print(num_a1 + num_d*(num_n-1))
